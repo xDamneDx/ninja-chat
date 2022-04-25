10b4b77eababa71296f348db5e7b3cc98170bf73
xDamneDx <xdamnedx@gmail.com>
2022-04-25 18:27:33 +0300

    Added sending new chats functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,17 @@
 const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
 
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom("general", "shaun");
+const chatroom = new Chatroom("general", "DamN");
+
+newChatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = newChatForm.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => newChatForm.reset())
+    .catch((err) => console.log(err));
+});
 
 chatroom.getChats((data) => chatUI.render(data));
